18537b7fd86f50c66c8086b4b1c25ad7fefdd320
medabbassi <mabbassix3@gmail.com>
2022-05-15 20:37:58 +0100

    first files

---
+++ compiler.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#the first function here 
+show_usage()
+{
+    echo "compiler.sh : [-h|--help] [-T] [-t] [-n] [-N] [-d] [-m] [-s] chemin.." ;
+
+}
+
---
+++ main.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+. ./compiler.sh  
+
+show_usage
