7d4b9273b580e1f64b3deffa0fd9239c1a7ed621
medabbassi <mabbassix3@gmail.com>
2022-05-17 22:35:57 +0100

    espace

--- compiler.sh
+++ compiler.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+ 
 #la fonction show_usage qui affiche sur la sortie standard le message
 show_usage()
 {
     echo "compiler.sh : [-h|--help] [-T] [-t] [-n] [-N] [-d] [-m] [-s] chemin.." ;
 
 }
+
 #tester la presence d'un argument ou affiche un erreur
 tester(){
 if [[ $# -eq 0 ]] ;
@@ -15,6 +16,7 @@ else
      echo "argument exist" ;
 fi
 }
+
 #onction nommée HELP qui permet d’afficher le help à partir d’n fichier texte 
  HELP(){
     cat ./help.txt;
